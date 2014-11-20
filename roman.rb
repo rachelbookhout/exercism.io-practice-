9261ba911bb27c524928d2873918e44b303b7108
rachelbookhout <rbookhou@gmail.com>
2014-11-20 11:33:15 -0500

    Set up the conversions

---
+++ roman.rb
@@ -0,0 +1,38 @@
+class Fixnum
+
+  ROMAN_NUM = {
+    1 => "I",
+    2 => "II",
+    3 => "III",
+    4 => "IV",
+    5 => "V",
+    6 => "VI",
+    7 => "VII",
+    8 => "VIII",
+    9 => "IX",
+    10 => "X",
+    50 => "L",
+    100 => "C",
+    500 => "D",
+    1000 => "M"
+  }
+
+
+  def to_roman
+
+  end
+end
+
+
+
+
+# In Roman numerals 1990 is MCMXC:
+
+# 1000=M
+# 900=CM
+# 90=XC
+
+# 2008 is written as MMVIII:
+
+# 2000=MM
+# 8=VIII

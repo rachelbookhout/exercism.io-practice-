e3648a79dc131e48e2ac93c92a7fb8e480eb37a9
rachelbookhout <rbookhou@gmail.com>
2014-11-27 09:48:56 -0500

    All tests for square function are passing

--- grains.rb
+++ grains.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Grains
-  def square(num)
-    (1..num)
+  def square(square_number)
+      return 2 ** (square_number -1)
+  end
+
+
+  def total
   end
 end
+
+
+  # [1,2,4,8,16,32,64]
+# We take the value, multiply it by 2.
+#Then we multiply it by two again, up to square_number times

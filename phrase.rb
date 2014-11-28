f7d9367fe0b7840178b4fe3e843430b635b24e48
rachelbookhout <rbookhou@gmail.com>
2014-11-27 20:35:33 -0500

    5 out of 9 tests pass

---
+++ phrase.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Phrase
+
+  def initialize(words)
+    @words = words
+  end
+
+  def word_count
+    @words = @words.downcase
+    @split_words = @words.split
+    counts = {}
+    @split_words.each do |word|
+      if counts[word] != nil
+        counts[word] = counts[word] + 1
+      else
+        counts[word] = 1
+      end
+    end
+   counts
+  end
+end

edd5e81804d25d521daf6a922ee48edf0ee94cf3
rachelbookhout <rbookhou@gmail.com>
2014-12-04 15:32:41 -0500

    Setting up the basics

---
+++ foodchain.rb
@@ -0,0 +1,20 @@
+class FoodChainSong
+  def initialize(song)
+    @song = song
+  end
+
+  def verse(num)
+    if num == 1
+      return "I know an old lady who swallowed a fly.
+              I don't know why she swallowed the fly. Perhaps she'll die."
+    else
+# I know an old lady who swallowed a #{animal}.
+#{comment about that animal}
+#{pattern over what got swallowed}
+#I don't know why she swallowed the fly. Perhaps she'll die.
+   end
+  end
+end
+
+
+

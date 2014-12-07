66e1c56ec4158eb1b31d3c388d4642cc5d0fe9a9
rachelbookhout <rbookhou@gmail.com>
2014-12-07 13:21:32 -0500

    researched the code

--- sieve.rb
+++ sieve.rb
@@ -1,16 +1,24 @@
 class Sieve
-  def initialize(num)
-    @num = num
+
+  attr_reader :range
+  def initialize(limit)
+    @range = (2..limit)
   end
 
   def primes
-    prime_num = []
-    2..@num.each do |number|
-      if !prime_num.empty?
-
-      else
-        prime_num << number
-      end
-    end
+    @primes ||= calculate
   end
+
+  private
+  def calculate
+    numbers = range.to_a
+    primes = []
+    begin
+      num = numbers.shift
+      primes << num
+      numbers.reject! {|i| i % num == 0 }
+    end until numbers.empty?
+    primes
+  end
+
 end

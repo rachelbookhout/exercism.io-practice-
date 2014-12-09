702218f83bb51b2c17a4ddd47e549c63982cf8b6
rachelbookhout <rbookhou@gmail.com>
2014-12-08 21:58:00 -0500

    Wrote code. Needs debugging. 2 out of 8 tests pass

---
+++ binary.rb
@@ -0,0 +1,16 @@
+class Binary
+
+  def initialize(number)
+    @number = number
+  end
+
+  def to_decimal
+    @number = @number.to_s.split.reverse
+    binary_num = 0
+    @number.each_with_index do |num,index|
+      binary_num += num.to_f * 10**index
+    end
+    binary_num
+  end
+
+end

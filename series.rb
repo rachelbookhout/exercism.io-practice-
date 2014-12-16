b5f2edd2d8e61912919a5168b91ddd6eb09cbc58
rachelbookhout <rbookhou@gmail.com>
2014-12-15 23:22:13 -0500

    created class

---
+++ series.rb
@@ -0,0 +1,14 @@
+class Series
+
+  def initalize(string)
+    @string = string
+  end
+
+  def slice(num)
+    #num is the chunk that each division needs to have in it
+    #It is coming from a string of numbers
+    #needs to return an array of array [[1],[2]] or [[1,2], [3,4]]
+    #slices are taken in order [[0,1,2],[1,2,3]]
+  end
+
+end

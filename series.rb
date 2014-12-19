7cc2ad72867ccb329881084421dbe0ee073d44e8
rachelbookhout <rbookhou@gmail.com>
2014-12-18 22:23:52 -0500

    still not working

--- series.rb
+++ series.rb
@@ -6,14 +6,15 @@ class Series
 
   def slices(num)
     sliced = []
-    while string.length > num
-      x = string.slice![0..num-1]
+    while @digits.length > num
+      x = digits.slice![0,num-1]
       sliced << x
     end
     #num is the chunk that each division needs to have in it
     #It is coming from a string of numbers
     #needs to return an array of array [[1],[2]] or [[1,2], [3,4]]
     #slices are taken in order [[0,1,2],[1,2,3]]
+    sliced
   end
 
  private

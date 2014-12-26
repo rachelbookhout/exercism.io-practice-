0d1818a14cda37e8990a4a4690029187333675fd
rachelbookhout <rbookhou@gmail.com>
2014-12-25 19:14:49 -0500

    Code does not work for multiple prime factors

--- prime_factors.rb
+++ prime_factors.rb
@@ -1,5 +1,20 @@
-class PrimeFactors
-  def for(num)
+require 'prime'
 
+class PrimeFactors
+
+  def self.for(num)
+    prime_factors = []
+    if num > 1
+    Prime.each(num) do |prime|
+      if num%prime==0
+        prime_factors << prime
+      end
+    end
+    return prime_factors
+  else
+    return prime_factors
   end
+  end
+
 end
+

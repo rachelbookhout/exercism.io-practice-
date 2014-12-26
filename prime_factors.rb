dc16362d623cdd213a79c63e0b0472afbff3187f
rachelbookhout <rbookhou@gmail.com>
2014-12-26 15:34:42 -0500

    All tests pass

--- prime_factors.rb
+++ prime_factors.rb
@@ -4,17 +4,16 @@ class PrimeFactors
 
   def self.for(num)
     prime_factors = []
-    if num > 1
-    Prime.each(num) do |prime|
-      if num%prime==0
-        prime_factors << prime
+    while num != 1
+      Prime.each(num) do |prime|
+        if num%prime==0
+          prime_factors << prime
+          num = num/prime
+          break
+        end
       end
     end
-    return prime_factors
-  else
-    return prime_factors
+  return prime_factors
   end
-  end
-
 end
 

bf5c59b8f370f6731b91a5d8e256c87258827f93
rachelbookhout <rbookhou@gmail.com>
2014-12-29 21:32:52 -0500

    Set up class and method

---
+++ etl.rb
@@ -0,0 +1,11 @@
+class ETL
+  def transform(old)
+    new_hash = {}
+
+    #value return is sorted by abcs
+    #if the hashes were different intially, only sorted within the indv. hashes
+    #the hash is reversed (a => 1)
+    #letter is downcased
+    #%w(foo bar) = ["foo", "bar"]
+  end
+end

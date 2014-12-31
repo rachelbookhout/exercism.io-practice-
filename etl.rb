5f15bd14abe4152b4d6b5f6a8f3f31619439606e
rachelbookhout <rbookhou@gmail.com>
2014-12-30 21:02:15 -0500

    All tests pass

--- etl.rb
+++ etl.rb
@@ -1,11 +1,11 @@
 class ETL
-  def transform(old)
+  def self.transform(old)
     new_hash = {}
-
-    #value return is sorted by abcs
-    #if the hashes were different intially, only sorted within the indv. hashes
-    #the hash is reversed (a => 1)
-    #letter is downcased
-    #%w(foo bar) = ["foo", "bar"]
+    old.each do |key, value|
+      value.each do |v|
+        new_hash[v.downcase] = key
+      end
+    end
+    return new_hash
   end
 end

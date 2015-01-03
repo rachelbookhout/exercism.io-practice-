80d2cb46d0b45ab946fcb98894de17a86bd7694a
rachelbookhout <rbookhou@gmail.com>
2015-01-02 21:21:07 -0500

    All but one tests pass

--- beer_song.rb
+++ beer_song.rb
@@ -7,9 +7,24 @@ class BeerSong
     elsif num == 1
       return "1 bottle of beer on the wall, 1 bottle of beer.\n" \
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif num == 2
+      return "2 bottles of beer on the wall, 2 bottles of beer.\n" \
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     else
       return "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" \
       "Take one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
     end
+  end
+
+  def verses(start,stop)
+    i = 0
+    until start - i < stop
+      verse(start)
+      i += 1
+    end
+  end
+
+  def sing
+    verses(99,0)
   end
 end

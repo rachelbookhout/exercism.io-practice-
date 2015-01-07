924ebc8093117ef250f2f2fedcfe05d41e971b4f
rachelbookhout <rbookhou@gmail.com>
2015-01-06 21:34:53 -0500

    6 out of 10 tests fail

---
+++ anagram.rb
@@ -0,0 +1,23 @@
+class Anagram
+
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+   anagrams = []
+   list.each do |item|
+
+    #if an anagram is found within the list
+    if item == word
+      anagrams << item
+    end
+    end #return the word
+  return anagrams
+
+  end
+end
+
+
+

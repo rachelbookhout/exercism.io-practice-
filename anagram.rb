633531ae168e28f86e7689e35ad12ff79e1e653b
rachelbookhout <rbookhou@gmail.com>
2015-01-09 21:13:16 -0500

    tests pass - review later

--- anagram.rb
+++ anagram.rb
@@ -1,21 +1,36 @@
 class Anagram
-
-  attr_reader :word
+  attr_reader :subject
   def initialize(word)
-    @word = word
+    @subject = AnagramSubject.new(word)
   end
 
-  def match(list)
-   anagrams = []
-   list.each do |item|
-
-    #if an anagram is found within the list
-    if item == word
-      anagrams << item
+  def match(candidates)
+    candidates.select do |candidate|
+      subject.anagram_of? candidate
     end
-    end
-  return anagrams
+  end
+end
 
+class AnagramSubject
+  attr_reader :subject
+  def initialize(subject)
+    @subject = subject
+  end
+
+  def anagram_of?(word)
+    !duplicate?(word) && fingerprint == canonicalize(word)
+  end
+
+  def duplicate?(word)
+    word.downcase == subject.downcase
+  end
+
+  def canonicalize(word)
+    word.downcase.chars.sort
+  end
+
+  def fingerprint
+    @fingerprint ||= canonicalize(subject)
   end
 end
 

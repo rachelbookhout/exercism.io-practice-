ee908658fe4d7c5b2b483a9a48de5da5a438cefa
rachelbookhout <rbookhou@gmail.com>
2015-01-13 22:01:35 -0500

    set up methods

---
+++ crypto.rb
@@ -0,0 +1,21 @@
+class Crypto
+  attr_reader :string
+  def initalize(string)
+    @string = string
+  end
+
+  def normalize_plaintext
+  end
+
+  def size
+  end
+
+  def plaintext_segments
+  end
+
+  def ciphertext
+  end
+
+  def normalize_ciphertext
+  end
+end

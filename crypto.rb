e99715355488f59ac0d2b3d346e756b8857af822
rachelbookhout <rbookhou@gmail.com>
2015-01-16 22:43:45 -0500

    wrote method. needs debugging

--- crypto.rb
+++ crypto.rb
@@ -11,14 +11,24 @@ class Crypto
   end
 
   def size
-    num = 1
-    until @string.size <= num*num
-      num +=1
+    @num = 1
+    until @string.size <= @num*@num
+      @num +=1
     end
-    return num
+    return @num
   end
 
   def plaintext_segments
+    square = []
+    @text=@string.normalize_plaintext
+    until @text.length = 0
+      if @text.length > @text.size
+        square << @text.slice!(0,@text.size - 1)
+      else
+        square << @text
+      end
+    end
+    return square
   end
 
   def ciphertext

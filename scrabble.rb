ac2c00f29e077862f903bb3532a888b71b10bb70
rachelbookhout <rbookhou@gmail.com>
2015-01-30 20:35:37 -0500

    scrabble values

--- scrabble.rb
+++ scrabble.rb
@@ -11,4 +11,12 @@
 
 class Scrabble
 
+  def initialize
+    values = {'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1,
+     'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1, 'D' => 2 , 'G' => 2, }
+  end
+
+  def score
+
+  end
 end

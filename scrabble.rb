d716a1afc008f576991823545ccff4bf5615d48b
rachelbookhout <rbookhou@gmail.com>
2015-02-01 13:57:03 -0500

    initialized the class

--- scrabble.rb
+++ scrabble.rb
@@ -11,16 +11,29 @@
 
 class Scrabble
 
-  def initialize(word)
-    @word = word
-    values = {'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1,
+  def self.score(term)
+    new(term).score
+  end
+
+  attr_reader :term
+
+  def initialize(term)
+    @term = term.to_s.downcase
+     @values = {'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1,
      'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1, 'D' => 2 , 'G' => 2,
       'B'=> 3, 'C' => 3, 'M'=> 3, 'P'=> 3,
       'F'=>4 , 'H'=>4 , 'V'=>4 , 'W'=>4 , 'Y'=> 4,
        'K' => 5, 'J' =>8, 'X' => 8,'Z'=>10 }
   end
 
-  def self.score
 
+
+  def score
+    letter_score = []
+    @word.each do |word|
+     score << @values[word]
+    end
+    score = letter_score.inject(0){|result, element| result + element}
+    return score
   end
 end

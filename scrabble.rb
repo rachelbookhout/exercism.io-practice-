111815760d23e9d4b1f9fe614424dd3968c1956c
rachelbookhout <rbookhou@gmail.com>
2015-02-06 21:15:16 -0500

    done

--- scrabble.rb
+++ scrabble.rb
@@ -1,28 +1,29 @@
 class Scrabble
-
   def self.score(term)
     new(term).score
   end
 
   attr_reader :term
-
   def initialize(term)
     @term = term.to_s.downcase
-     @values = {'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1,
-     'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1, 'D' => 2 , 'G' => 2,
-      'B'=> 3, 'C' => 3, 'M'=> 3, 'P'=> 3,
-      'F'=>4 , 'H'=>4 , 'V'=>4 , 'W'=>4 , 'Y'=> 4,
-       'K' => 5, 'J' =>8, 'X' => 8,'Z'=>10 }
   end
 
-
-
   def score
-    letter_score = []
-    @term.each_char do |word|
-     score << @values[word]
+    sum = 0
+    term.chars.each do |char|
+      sum += letter_values.fetch(char) { 0 }
     end
-    score = letter_score.inject(0){|result, element| result + element}
-    return score
+    sum
+  end
+
+  def letter_values
+    {
+      'a' => 1, 'b' => 3, 'c' => 3, 'd' => 2, 'e' => 1,
+      'f' => 4, 'g' => 2, 'h' => 4, 'i' => 1, 'j' => 8,
+      'k' => 5, 'l' => 1, 'm' => 3, 'n' => 1, 'o' => 1,
+      'p' => 3, 'q' => 10, 'r' => 1, 's' => 1, 't' => 1,
+      'u' => 1, 'v' => 4, 'w' => 4, 'x' => 8, 'y' => 4,
+      'z' => 10
+    }
   end
 end
